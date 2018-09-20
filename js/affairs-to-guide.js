9c7351e799aa1f38f22edda3abdee3450e23ea9c
DESKTOP-5K7FJ6E\thunder <1652575963@qq.com>
2018-09-20 18:47:41 +0800

    专题专栏数据对接

--- js/affairs-to-guide.js
+++ js/affairs-to-guide.js
@@ -1,36 +1,79 @@
+var page0 = page1 = page2 = 1;
 var app = new Vue({
 	el: '#app',
 	data: {
+		//党内政策 28
+        noticeObj:{
+            noticeList:[
 
+            ],
+            isShow:false
+        },
+        //法规 29
+        annouceObj:{
+            announcementList:[
+
+            ],
+            isShow:false
+        },
+        //制度 30
+        publicityObj:{
+            publicityList:[
+
+            ],
+            isShow:false
+        }
 	},
 	methods: {
-		readAllNotice: function () {
-			$('.notice .article-content').css({
-				'overflow-y': 'scroll'
-			});
-			$('.notice .read-all-link').hide();
-			$('.notice .article-content').css({
-				'height': '100%'
-			});
-		},
-		readAllAnnouncement: function () {
-			$('.announcement .article-content').css({
-				'overflow-y': 'scroll'
-			});
-			$('.announcement .read-all-link').hide();
-			$('.announcement .article-content').css({
-				'height': '100%'
-			});
-		},
-		readAllPublicity: function () {
-			$('.publicity .article-content').css({
-				'overflow-y': 'scroll'
-			});
-			$('.publicity .read-all-link').hide();
-			$('.publicity .article-content').css({
-				'height': '100%'
-			});
-		}
+        readAllNotice: function (isFirst) {
+            if(isFirst) {
+                $('.notice .article-content').css({
+                    'overflow-y': 'scroll'
+                });
+                $('.notice .bottom-read-btn .read-all-link').hide();
+                $('.notice .article-content').css({
+                    'height': '100%'
+                });
+            }
+            else {
+                //请求下一页数据
+                getNotice(++page0);
+            }
+        },
+        readAllAnnouncement: function (isFirst) {
+            if(isFirst) {
+                $('.announcement .article-content').css({
+                    'overflow-y': 'scroll'
+                });
+                $('.announcement .bottom-read-btn .read-all-link').hide();
+                $('.announcement .article-content').css({
+                    'height': '100%'
+                });
+            }
+            else {
+                //请求下一页数据
+                getAnn(++page1);
+            }
+        },
+        readAllPublicity: function (isFirst) {
+            if(isFirst) {
+                $('.publicity .article-content').css({
+                    'overflow-y': 'scroll'
+                });
+                $('.publicity .bottom-read-btn .read-all-link').hide();
+                $('.publicity .article-content').css({
+                    'height': '100%'
+                });
+            }
+            else {
+                //请求下一页数据
+                getPub(++page2);
+            }
+        },
+        //跳转链接
+        goDetail:function (id,type) {
+            window.location.href = 'branch-life-inner.html?id=' + id + '&type=' + type;
+        }
 	}
 });
 
@@ -45,6 +88,12 @@ $(function () {
 			$("div.container").find("div.main-content").eq(index).addClass('active');
 		}
 	});
+    /**
+     * 进入页面加载数据
+     */
+    getNotice(1);
+    getAnn(1);
+    getPub(1);
 });
 
 
@@ -54,4 +103,59 @@ function goHome() {
 
 function goBack() {
 	window.history.back(-1);
+}
+
+
+//获取党内政策数据
+function getNotice(page) {
+    getData(rootUrl+'index/index',28,page);
+}
+//获取法规数据
+function getAnn(page) {
+    getData(rootUrl+'index/index',29,page);
+}
+//获取制度数据
+function getPub(page) {
+    getData(rootUrl+'index/index',30,page);
+}
+
+//获取数据通用函数
+function getData(url,type,page) {
+    $.ajax({
+        url:url,
+        data:{
+            pid:type,
+            p:page
+        },
+        type:'post',
+        dataType:'json',
+        success:function(data) {
+            switch (type) {
+                case 28:
+                    //通知数据变更
+                    app.noticeObj.noticeList = app.noticeObj.noticeList.concat(data.data.list);
+                    if(data.data.total > 10) {
+                        app.noticeObj.isShow = true;
+                    }
+                    break;
+                case 29:
+                    //公告数据变更
+                    app.annouceObj.announcementList = app.annouceObj.announcementList.concat(data.data.list);
+                    if(data.data.total > 10) {
+                        app.annouceObj.isShow = true;
+                    }
+                    break;
+                case 30:
+                    //公示数据变更
+                    app.publicityObj.publicityList = app.publicityObj.publicityList.concat(data.data.list);
+                    if(data.data.total > 10) {
+                        app.publicityObj.isShow = true;
+                    }
+                    break;
+            }
+        },
+        error:function() {
+
+        }
+    })
 }
--- js/branch-life.js
+++ js/branch-life.js
@@ -1,121 +1,241 @@
+var page0 = page1 = page2 = page3 = 1;
 var app = new Vue({
-	el: '#app',
-	data: {
-		images: [
-            'img/index/broadcast/1.jpg',
-            'img/index/broadcast/2.jpg',
-            'img/index/broadcast/3.jpg',
-            'img/index/broadcast/4.jpg',
-            'img/index/broadcast/5.jpg'
-		],
-		orgList:[],
-		workList:[]
-	},
-	methods: {
-		readAllOrg: function () {
-			$('.org .article-content').css({
-				'overflow-y': 'scroll'
-			});
-			$('.org .read-all-link').hide();
-			$('.org .article-content').css({
-				'height': '100%'
-			});
-		},
-		readAllArticle:function() {
-			$('.work .article-content').css({
-				'overflow-y': 'scroll'
-			});
-			$('.work .read-all-link').hide();
-			$('.work .article-content').css({
-				'height': '100%'
-			});
-		},
-		orgInfo:function(id) {
-			window.location.href = 'branch-life-inner.html?id=' + id;
-		},
-		workDync:function(id) {
-			window.location.href = 'branch-life-inner.html?id=' + id;
-		}
-	}
+    el: '#app',
+    data: {
+        branch: '',
+        partyMemberList: [],
+        branchActList: {
+            list: [],
+            isMore: true
+        },
+        orgObj: {
+            orgList: [],
+            isShow: false,
+            isHideButton: false
+        },
+        workObj: {
+            workList: [],
+            isShow: false
+        }
+    },
+    methods: {
+        readAllOrg: function (isFirst) {
+            if (isFirst) {
+                $('.org .article-content').css({
+                    'overflow-y': 'scroll'
+                });
+                $('.org .bottom-read-btn .read-all-link').hide();
+                $('.org .article-content').css({
+                    'height': '100%'
+                });
+            }
+            else {
+                //请求下一页数据
+                getOrgData(++page0, app.branch, '');
+            }
+        },
+        readAllWork: function (isFirst) {
+            if (isFirst) {
+                $('.work .article-content').css({
+                    'overflow-y': 'scroll'
+                });
+                $('.work .bottom-read-btn .read-all-link').hide();
+                $('.work .article-content').css({
+                    'height': '100%'
+                });
+            }
+            else {
+                //请求下一页数据
+                getWorkData(++page1, 0, 34);
+            }
+        },
+        readAllBranch:function() {
+            getBrancActhData(++page3, app.branch, 36);
+        },
+        orgInfo: function (id) {
+            window.location.href = 'branch-life-inner.html?id=' + id;
+        },
+        workDync: function (id) {
+            window.location.href = 'branch-life-inner.html?id=' + id;
+        },
+        goInner: function (id) {
+            app.branch = id;
+            //组织架构点击事件
+            app.orgObj.orgList = [];
+            app.orgObj.isShow = false;
+            getOrgData(1, id, '');
+        },
+        goDetail:function (id,type) {
+            window.location.href = 'branch-life-inner.html?id=' + id + '&type=' + type;
+        }
+    }
 });
 
 $(function () {
-	$("div.class-btn-group img").click(function () {
-		var img = $(this);
-		if (!img.hasClass('scale-img')) {
-			var index = $.inArray(img[0], $('div.class-btn-group img'));
-			$(this).addClass('scale-img');
-			$(this).siblings().removeClass('scale-img');
-			$("div.container").find("div.main-content").removeClass('active');
-			$("div.container").find("div.main-content").eq(index).addClass('active');
-		}
-	});
+    $("div.class-btn-group img").click(function (ev) {
+        //重置页码和数据
+        page0 = page1 = page2 = page3 = 1;
+        app.orgObj.orgList = [];
+        app.orgObj.isShow = false;
+        app.workObj = {
+            workList: [],
+            isShow: false
+        };
+        var img = $(this);
+        if (!img.hasClass('scale-img')) {
+            var index = $.inArray(img[0], $('div.class-btn-group img'));
+            $(this).addClass('scale-img');
+            $(this).siblings().removeClass('scale-img');
+            $("div.container").find("div.main-content").removeClass('active');
+            $("div.container").find("div.main-content").eq(index).addClass('active');
+        }
+        //发送请求
+        switch ($(ev.target).attr("data-id")) {
+            case "":
+                getOrgData(1, 0, '');
+                break;
+            case "34":
+                getWorkData(1, app.branch, 34);
+                break;
+            case "35":
+                getWorkData(1, app.branch, 35);
+                break;
+            case "36":
+                getWorkData(1, app.branch, 36);
+                break;
+        }
+    });
 
-	/**
-	 * 初始化轮播组件
-	 */
-	Carousel.init($("#carousel-image"));
-	$("#carousel-image").init();
-	//触摸事件
-	var x_orgin = 0;
-	var x_now = 0;
-	var isTouch = false;
-	document.querySelector('#carousel-image').addEventListener('touchstart', function (ev) {
-		x_origin = ev.touches[0].clientX;
-		$(this).bind('touchmove', function (ev) {
-			isTouch = true;
-			x_now = ev.originalEvent.touches[0].clientX;
-		});
-	});
-	document.querySelector('#carousel-image').addEventListener('touchend', function () {
-		if (isTouch) {
-			if (x_now - x_origin < 0) {
-				$('#carousel-image').find('.poster-next-btn').click();
-			} else {
-				$('#carousel-image').find('.poster-prev-btn').click();
-			}
-		}
-		isTouch = false;
-	});
+    /**
+     * 获取数据
+     */
+    getOrgData(1, 0, '');
+    /**
+     * 监听图片容器滚动
+     */
+    $('#branch-activity').scroll(function () {
+        console.log($(this).scrollLeft());
+    });
 });
 
-
+/**
+ * 初始化轮播组件
+ */
+function initPlugin() {
+    Carousel.init($("#carousel-image"));
+    //触摸事件
+    var x_orgin = 0;
+    var x_now = 0;
+    var isTouch = false;
+    document.querySelector('#carousel-image').addEventListener('touchstart', function (ev) {
+        x_origin = ev.touches[0].clientX;
+        $(this).bind('touchmove', function (ev) {
+            isTouch = true;
+            x_now = ev.originalEvent.touches[0].clientX;
+        });
+    });
+    document.querySelector('#carousel-image').addEventListener('touchend', function () {
+        if (isTouch) {
+            if (x_now - x_origin < 0) {
+                $('#carousel-image').find('.poster-next-btn').click();
+            } else {
+                $('#carousel-image').find('.poster-prev-btn').click();
+            }
+        }
+        isTouch = false;
+    });
+}
 function goHome() {
-	window.location.href = 'index.html';
+    window.location.href = 'index.html';
 }
 
 function goBack() {
-	window.history.back(-1);
+    window.history.back(-1);
 }
 
 /**
  * 获取组织架构数据
  */
-function getOrgData() {
-
+function getOrgData(page, branch, type) {
+    getData(rootUrl + 'index/branchLife', page, branch, type);
 }
 
 /**
  * 获取工作动态数据
  */
-function getOrgData() {
-
+function getWorkData(page, branch, type) {
+    getData(rootUrl + 'index/branchLife', page, branch, type);
 }
 
 /**
  * 获取党员风采数据
  */
-function getOrgData() {
-
+function getDangData(page, branch, type) {
+    getData(rootUrl + 'index/branchLife', page, branch, type);
 }
 
 /**
  * 获取支部活动数据
  */
-function getOrgData() {
-
+function getBrancActhData(page, branch, type) {
+    getData(rootUrl + 'index/branchLife', page, branch, type);
 }
 
-function getData() {
-	
+function getData(url, page, branch, type) {
+    $.ajax({
+        url: url,
+        data: {
+            p: page,
+            branch: branch,
+            type: type
+        },
+        type: 'post',
+        dataType: 'json',
+        success: function (data) {
+            switch (type) {
+                case '':
+                    //组织架构数据变更
+                    app.orgObj.orgList = app.orgObj.orgList.concat(data.data.list);
+                    if (data.data.total > 10) {
+                        app.orgObj.isShow = true;
+                    }
+                    if (data.data.names) {
+                        app.orgObj.isHideButton = true;
+                        getWorkData(1, branch, 34);
+                    }
+                    break;
+                case 34:
+                    //工作动态数据变更
+                    app.workObj.workList = app.workObj.workList.concat(data.data.list);
+                    if (data.data.total > 10) {
+                        app.workObj.isShow = true;
+                    }
+                    break;
+                case 35:
+                    //党员风采数据变更
+                    app.partyMemberList = data.data.list.slice(0,5);
+                    console.log(app.partyMemberList);
+                    //加载轮播组件
+                    setTimeout(function() {
+                        initPlugin();
+                    },0);
+                    break;
+                case 36:
+                    //支部活动数据变更
+                    app.branchActList.list = app.branchActList.list.concat(data.data.list);
+                    if (data.data.total > 10) {
+                        app.branchActList.isMore = true;
+                    }
+                    break;
+            }
+        },
+        error: function () {
+
+        }
+    })
+}
+
+function goDetail(elem) {
+    window.event.returnValue=false;
+    window.location.href = 'branch-life-inner.html?id=' + $(elem).attr("id") + '&type=36';
 }
